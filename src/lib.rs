5dd3851996230da7cef0dbdac1bd65334d7f6e97
Mohammad Mustakim Ali <mustakimali@users.noreply.github.com>
2021-10-26 00:22:03 +0100

    more work

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,9 @@ pub mod mock_server;
 pub use mock_server::*;
 #[cfg(test)]
 mod tests {
-    use std::time::Duration;
+    use std::{net::TcpStream, time::Duration};
 
-    use tonic::Status;
+    use tonic::Code;
 
     use crate::{
         greeter_code::{greeter_client, HelloReply, HelloRequest},
@@ -14,18 +14,23 @@ mod tests {
     };
 
     #[tokio::test]
+    async fn it_starts_with_specified_port() {
+        let server = MockGrpcServer::new(50055).start().await;
+
+        assert!(TcpStream::connect(&server.address()).is_ok())
+    }
+
+    #[tokio::test]
     async fn it_works() {
         // Server
-        let server = MockGrpcServer::new(50055).start();
+        let mut server = MockGrpcServer::new(50055).start().await;
         tokio::time::sleep(Duration::from_secs(1)).await;
 
         server.setup(
             RequestBuilder::given("/")
-                .return_status(Status::ok(""))
-                .return_body(|| {
-                    Ok(HelloReply {
-                        message: "yo".into(),
-                    })
+                .return_status(Code::Ok)
+                .return_body(|| HelloReply {
+                    message: "yo".into(),
                 }),
         );
 
@@ -37,12 +42,14 @@ mod tests {
         let mut client = greeter_client::GreeterClient::new(channel);
 
         // Act
-        let _r = client
+        let response = client
             .say_hello(HelloRequest {
                 name: "Yo yo".into(),
             })
             .await
             .unwrap();
+
+        assert_eq!("", response.into_inner().message);
     }
 
     #[test]
--- src/mock_server.rs
+++ src/mock_server.rs
@@ -1,45 +1,72 @@
+use log::debug;
 use prost::Message;
-use std::{sync::Arc, task::Poll};
-use tonic::codegen::{http, Body, Never, StdError};
+use std::{
+    net::{IpAddr, Ipv4Addr, SocketAddr, TcpStream},
+    sync::{Arc, RwLock},
+    task::Poll,
+    time::Duration,
+};
+use tonic::{
+    codegen::{http, Body, Never, StdError},
+    Code, Status,
+};
 
 #[derive(Clone)]
 pub struct MockGrpcServer {
-    port: u32,
+    address: SocketAddr,
     inner: Arc<Option<Inner>>,
+    rules: Arc<RwLock<Vec<RequestBuilder>>>,
 }
 
 struct Inner {
     #[allow(dead_code)]
     join_handle: tokio::task::JoinHandle<Result<(), tonic::transport::Error>>,
-    rules: Vec<RequestBuilder>,
 }
 
 impl MockGrpcServer {
-    pub fn new(port: u32) -> Self {
+    pub fn new(port: u16) -> Self {
         Self {
-            port,
+            address: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port),
             inner: Arc::default(),
+            rules: Arc::default(),
         }
     }
 
-    pub fn start(mut self) -> Self {
+    pub async fn start(mut self) -> Self {
+        debug!("Starting gRPC started in {}", self.address());
+
         let thread = tokio::spawn(
             tonic::transport::Server::builder()
                 .add_service(self.clone())
-                .serve(format!("[::1]:{}", self.port).parse().unwrap()),
+                .serve(format!("[::1]:{}", self.address.port()).parse().unwrap()),
         );
 
         self.inner = Arc::new(Some(Inner {
             join_handle: thread,
-            rules: Vec::default(),
         }));
+
+        for _ in 0..40 {
+            if TcpStream::connect_timeout(&self.address, std::time::Duration::from_millis(25))
+                .is_ok()
+            {
+                break;
+            }
+            debug!("WAITING...");
+            tokio::time::sleep(Duration::from_millis(25)).await;
+        }
+
+        debug!("Server started in {}", self.address());
         self
     }
 
-    pub fn setup<T: prost::Message>(self, r: RequestBuilder) -> MockGrpcServer {
-        r.mount(&self);
+    pub fn setup(&mut self, r: RequestBuilder) -> MockGrpcServer {
+        r.mount(self);
 
-        self
+        self.to_owned()
+    }
+
+    pub fn address(&self) -> &SocketAddr {
+        &self.address
     }
 }
 
@@ -63,22 +90,38 @@ where
     fn call(&mut self, req: http::Request<B>) -> Self::Future {
         println!("Request to {}", req.uri().path());
 
-        match req.uri().path() {
-            _ => Box::pin(async move {
-                Ok(http::Response::builder()
-                    .status(200)
-                    .header("grpc-status", "12")
-                    .header("content-type", "application/grpc")
-                    .body(tonic::body::empty_body())
-                    .unwrap())
-            }),
+        let path = req.uri().path();
+        let inner = self.rules.as_ref();
+        if let Some(req_builder) = inner.read().unwrap().iter().find(|x| x.path == path) {
+            let builder = http::Response::builder()
+                .status(200)
+                .header("content-type", "application/grpc")
+                .header(
+                    "grpc-status",
+                    Status::new(req_builder.status_code.unwrap_or(Code::Ok), "").to_string(),
+                );
+
+            if let Some(body) = &req_builder.result {
+                builder.body(body).unwrap();
+            } else {
+                builder.body(tonic::body::empty_body()).unwrap();
+            }
         }
+
+        Box::pin(async move {
+            Ok(http::Response::builder()
+                .status(200)
+                .header("grpc-status", "12")
+                .header("content-type", "application/grpc")
+                .body(tonic::body::empty_body())
+                .unwrap())
+        })
     }
 }
 
 pub struct RequestBuilder {
     path: String,
-    status_code: Option<tonic::Status>,
+    status_code: Option<tonic::Code>,
     result: Option<Vec<u8>>,
 }
 
@@ -95,7 +138,7 @@ impl RequestBuilder {
         todo!()
     }
 
-    pub fn return_status(self, status: tonic::Status) -> Self {
+    pub fn return_status(self, status: tonic::Code) -> Self {
         Self {
             status_code: Some(status),
             ..self
@@ -116,11 +159,11 @@ impl RequestBuilder {
         }
     }
 
-    pub fn mount(self, s: &MockGrpcServer) {
+    pub fn mount(self, s: &mut MockGrpcServer) {
         if self.status_code.is_none() && self.result.is_none() {
             panic!("Must set the status code or body before attempting to mount the rule.");
         }
 
-        //s.inner.unwrap().rules.push(Box::new(self));
+        s.rules.write().unwrap().push(self);
     }
 }
