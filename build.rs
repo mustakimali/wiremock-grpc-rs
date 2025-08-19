67ab49e4b7568c83716fc4586e38cb0ab6b69441
Joseph Livesey <joseph@semiotic.ai>
2025-08-19 07:59:05 -0400

    build: update protobuf crate to 0.14.1 (#8)
    Signed-off-by: Joseph Livesey <joseph@semiotic.ai>

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/hello.proto")?;
+    tonic_prost_build::compile_protos("proto/hello.proto")?;
 
     Ok(())
 }
